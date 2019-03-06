1254d57c1d9be802150734e1e764ee24f9569d89
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2019-03-06 21:12:47 +0000

    [modlog] Un solo modal; nueva API

--- modlog/js/modlog.js
+++ modlog/js/modlog.js
@@ -5,26 +5,32 @@
     data: {
       loading: true,
       error: false,
-      modActions: []
+      modActions: [],
+      selectedAction: null,
+      details: function(action, e) {
+        if (e) e.preventDefault();
+        app.selectedAction = action;
+        M.Modal.getInstance(d.querySelector('#entry-modal')).open();
+      }
     },
     updated: function(){ this.$nextTick(() => {
       M.Collapsible.init(d.querySelectorAll('.collapsible'));
-      M.Modal.init(d.querySelectorAll('.modal'));
     })},
     mounted: function () {
       d.querySelector("#app").style.visibility = 'visible';
+      M.Modal.init(d.querySelectorAll('.modal'));
     }
   });
 
   function loadEntries(after) {
-    let endpoint = 'https://modlog.rchile.xyz/';
+    let endpoint = 'https://modlog2.rchile.xyz/entries';
     if (after) {
-      endpoint += 'after/' + after;
+      endpoint += '/after/' + after;
     }
 
     app.loading = true;
     allowLoad = false;
-    axios.post(endpoint, {}).then(resp => {
+    axios.get(endpoint, {}).then(resp => {
       app.loading = false;
       allowLoad = true;
 
@@ -45,8 +51,7 @@
 
     let trigger = (scrollY + (w.innerHeight * 1.7)) > docHeight();
     if (trigger && nModActions > 0) {
-      let id = app.modActions[nModActions - 1].entry.id.split('_')[1];
-      loadEntries(id);
+      loadEntries(app.modActions[nModActions - 1].entry.id);
     }
   });
 
