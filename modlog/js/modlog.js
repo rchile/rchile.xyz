6116894de5c8e86e8c348fb4d232152a0d1b2b40
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2019-04-22 21:42:27 +0000

    [modlog] Forgot again to update API url

--- modlog/js/modlog.js
+++ modlog/js/modlog.js
@@ -2,8 +2,8 @@
   'use strict';
   let allowLoad = true;
   let timeagoIns = timeago();
-  //let API = 'https://modlog.rchile.xyz';
-  let API = 'http://127.0.0.1:5000';
+  let API = 'https://modlog.rchile.xyz';
+  //let API = 'http://127.0.0.1:5000';
 
   let app = new Vue({
     el: '#app',
